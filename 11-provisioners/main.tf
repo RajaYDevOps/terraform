cb117cf715612bda91fa3865b8ed250925e38b71
RajaYDevOps <crr.yannam90@gmail.com>
2026-08-27 12:10:38 +0530

    terraform provionsers

--- 11-provisioners/main.tf
+++ 11-provisioners/main.tf
@@ -18,6 +18,32 @@ tags = {
     when = destroy
     command = "echo > inventory.ini"
   }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    password = "DevOps321"
+    host        = self.public_ip
+  }
+
+   provisioner "remote-exec" {
+    inline = [
+       "sudo dnf install nginx -y",
+       "sudo systemctl start nginx"
+    ]
+  }
+
+    provisioner "remote-exec" {
+     when = destroy
+     inline = [
+       "sudo systemctl stop nginx"
+    ]
+  }
+
+    /* provisioner "file-exec"{
+      source = "script.sh"
+      destination = "/tmp/script.sh"
+    } */
 }
 
 #It creates default vpc in case if we dont mention any
@@ -30,6 +56,20 @@ resource "aws_security_group" "allow_terraform" {
     from_port        = 0
     to_port          = 0
     protocol         = "-1" #all traffic
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress{
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp" #all traffic
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress{
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp" #all traffic
     cidr_blocks      = ["0.0.0.0/0"]
   }
   tags = {
