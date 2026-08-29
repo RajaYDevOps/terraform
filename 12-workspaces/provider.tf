a478eb6826e5d0d08b58598ce3937126bddd4995
RajaYDevOps <crr.yannam90@gmail.com>
2026-08-29 12:44:46 +0530

    terraform workspace

---
+++ 12-workspaces/provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.54"
+    }
+  }
+
+#remote state storage location
+backend "s3" {
+    bucket         = "remote-state-file-90s"
+    key            = "workspace-demo.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    
+    # Enable native S3 state locking (Available in Terraform 1.10+)
+    use_lockfile   = true 
+  }
+
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
